2e1e653854a7faabdba7fafec2088ccdbcc1eb9c
genan566 <avodagbejean@gmail.com>
2024-02-27 14:05:41 +0100

    feat: front review

--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -28,13 +28,7 @@ export class NewsService {
   getData(): News[] {
     return this.newsPull;
   }
-
-  /**
-   * Handle Http operation that failed.
-   * Let the app continue.
-   * @param operation - name of the operation that failed
-   * @param result - optional value to return as the observable result
-   */
+  
   private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
       // TODO: send the error to remote logging infrastructure
